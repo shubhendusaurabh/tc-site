98c77e060fa2073ccfc5323e9b6918796753851c
vikasrohit <vikasrohit4@yahoo.com>
2015-04-06 11:54:52 +0530

    SUP-236, Build changes for integrating common header/footer into apps external to wordpress
    Removed changes specific to local environment.

--- Gruntfile.js
+++ Gruntfile.js
@@ -35,7 +35,7 @@ module.exports = function(grunt) {
     auth0LDAP: grunt.option('auth-ldap') || 'LDAP',
     auth0URL: grunt.option('auth-main-url') || 'topcoder.auth0.com',
     mainURL: grunt.option('main-url') || 'http://local.topcoder.com',
-    apiURL: grunt.option('api-url') || 'https://api.topcoder-dev.com/v2',
+    apiURL: grunt.option('api-url') || 'https://api.topcoder.com/v2',
     cdnURL: grunt.option('cdn-url') || '/mf',
     useVer: grunt.option('use-ver') || false,
     version: grunt.option('cdn-version') || '',
@@ -54,7 +54,7 @@ module.exports = function(grunt) {
     myFiltersURL: grunt.option('my-filters-url') || '//lc1-user-settings-service.herokuapp.com',
     cbURL: grunt.option('cb-url') || 'https://coderbits.com',
 
-    blogRSSFeedURL: grunt.option('blog-rss-feed') || 'http://local.topcoder.com/feed/?post_type=blog',
+    blogRSSFeedURL: grunt.option('blog-rss-feed') || 'https://wwww.topcoder.com/feed/?post_type=blog',
     photoLinkBaseURL: grunt.option('photo-link-base') || 'http://community.topcoder.com',
 
     marketingMessageMyDashURL: grunt.option('marketing-message-my-dash-url') || 'https://banners-r-us.herokuapp.com/serve?size=650x150'
